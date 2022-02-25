d1ecad731b46369d3a9f24c7190a26f52881b6ce
KarolinaBrzezinska <karozablo@gmail.com>
2022-02-25 01:27:23 +0100

    added gallery without modal btn

--- 07/src/js/01-gallery.js
+++ 07/src/js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+const $gallery = document.querySelector(".gallery");
+
+const imagePreview = [];
+const imageOriginal = [];
+const imageAlt = [];
+
+for (const item of galleryItems) {
+	imagePreview.push(item.preview);
+	imageOriginal.push(item.original);
+	imageAlt.push(item.description);
+}
+
+for (let i = 0; i < galleryItems.length; i++) {
+	$gallery.insertAdjacentHTML(
+		"afterbegin",
+		`<div class="gallery__item"><a class="gallery__link" href="${imageOriginal[i]}"><img class="gallery__image" src ="${imagePreview[i]}" data-source="${imageOriginal[i]}"alt="${imageAlt[i]}"  ></a></div>`
+	);
+}
