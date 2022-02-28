5bc6bafda716941a011efdad13ecf5edfda70a6c
KarolinaBrzezinska <karozablo@gmail.com>
2022-03-01 00:58:47 +0100

    edit

--- 07/src/js/01-gallery.js
+++ 07/src/js/01-gallery.js
@@ -1,38 +1,40 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const createGalleryHTML = [];
 const $div = document.querySelector("div");
 
+// puste tablice na pogrupowane właściwościami image
 const imagePreview = [];
 const imageOriginal = [];
 const imageAlt = [];
 
+//pusta tablica na kod HTML 
+const createGalleryHTML = [];
+
+// uzupełnienie powyższych tablic
 for (const item of galleryItems) {
 	imagePreview.push(item.preview);
 	imageOriginal.push(item.original);
 	imageAlt.push(item.description);
 }
 
+// stworzenie kodu HTML
 for (let i = 0; i < galleryItems.length; i++) {
 	const newHTML = `<div class="gallery__item"><a class="gallery__link" href="${imageOriginal[i]}"><img class="gallery__image" src ="${imagePreview[i]}" data-source="${imageOriginal[i]}"alt="${imageAlt[i]}"  ></a></div>`;
 
 	createGalleryHTML.push(newHTML);
 	}
 
+// przekształcenie tablicy w ciąg znaków
 const readyHTMLToPushToWebsite = createGalleryHTML.join(" ");
+
+//dodanie na stronę fragmentu kodu, bez pętli
 $div.insertAdjacentHTML("afterbegin", readyHTMLToPushToWebsite)
 
-
-// zablokowane przeniesienie na stronę
-
-
+// zablokowane przeniesienie na stronę po kliknięciu w obrazek
 window.addEventListener("click", (ev) => {
 	const element = ev.target;
 	const $nearest = element.closest(".gallery__link")
 	console.log($nearest.href);
-
 	ev.preventDefault();
-
-	// console.log(ev.target)
 })
 
