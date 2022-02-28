f1ff7cad411054729a37f5ce54d8bafd477b35a1
KarolinaBrzezinska <karozablo@gmail.com>
2022-03-01 00:54:02 +0100

    zablokowane przenoszenie na inną stronę

--- 07/src/js/01-gallery.js
+++ 07/src/js/01-gallery.js
@@ -13,19 +13,26 @@ for (const item of galleryItems) {
 	imageAlt.push(item.description);
 }
 
-
-
 for (let i = 0; i < galleryItems.length; i++) {
 	const newHTML = `<div class="gallery__item"><a class="gallery__link" href="${imageOriginal[i]}"><img class="gallery__image" src ="${imagePreview[i]}" data-source="${imageOriginal[i]}"alt="${imageAlt[i]}"  ></a></div>`;
 
 	createGalleryHTML.push(newHTML);
 	}
 
-const ready = createGalleryHTML.join(" ");
-
-$div.insertAdjacentHTML("afterbegin", ready)
-
+const readyHTMLToPushToWebsite = createGalleryHTML.join(" ");
+$div.insertAdjacentHTML("afterbegin", readyHTMLToPushToWebsite)
 
 
+// zablokowane przeniesienie na stronę
 
+
+window.addEventListener("click", (ev) => {
+	const element = ev.target;
+	const $nearest = element.closest(".gallery__link")
+	console.log($nearest.href);
+
+	ev.preventDefault();
+
+	// console.log(ev.target)
+})
 
