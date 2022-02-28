4bf911dcc612ef8f15ff70f538181f0f4066ca5e
KarolinaBrzezinska <karozablo@gmail.com>
2022-03-01 00:05:37 +0100

    dodanie elementu na strone bez pętli

--- 07/src/js/01-gallery.js
+++ 07/src/js/01-gallery.js
@@ -1,8 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-//console.log(galleryItems);
-const $gallery = document.querySelector(".gallery");
+const createGalleryHTML = [];
+const $div = document.querySelector("div");
 
 const imagePreview = [];
 const imageOriginal = [];
@@ -14,9 +13,19 @@ for (const item of galleryItems) {
 	imageAlt.push(item.description);
 }
 
+
+
 for (let i = 0; i < galleryItems.length; i++) {
-	$gallery.insertAdjacentHTML(
-		"afterbegin",
-		`<div class="gallery__item"><a class="gallery__link" href="${imageOriginal[i]}"><img class="gallery__image" src ="${imagePreview[i]}" data-source="${imageOriginal[i]}"alt="${imageAlt[i]}"  ></a></div>`
-	);
-}
+	const newHTML = `<div class="gallery__item"><a class="gallery__link" href="${imageOriginal[i]}"><img class="gallery__image" src ="${imagePreview[i]}" data-source="${imageOriginal[i]}"alt="${imageAlt[i]}"  ></a></div>`;
+
+	createGalleryHTML.push(newHTML);
+	}
+
+const ready = createGalleryHTML.join(" ");
+
+$div.insertAdjacentHTML("afterbegin", ready)
+
+
+
+
+
