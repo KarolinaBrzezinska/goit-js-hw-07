02e35c4bd550b017176d1835484cf872046fa80a
KarolinaBrzezinska <karozablo@gmail.com>
2022-03-08 00:08:01 +0100

    Update 01-gallery.js

--- 07/src/js/01-gallery.js
+++ 07/src/js/01-gallery.js
@@ -2,31 +2,24 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const $div = document.querySelector("div.gallery");
 
-// puste tablice na pogrupowane właściwościami image
 const imagePreview = [];
 const imageOriginal = [];
 const imageAlt = [];
-
-//pusta tablica na kod HTML
 const createGalleryHTML = [];
 
-// uzupełnienie powyższych tablic
 for (const item of galleryItems) {
   imagePreview.push(item.preview);
   imageOriginal.push(item.original);
   imageAlt.push(item.description);
 }
 
-// stworzenie kodu HTML
 for (let i = 0; i < galleryItems.length; i++) {
   const newHTML = `<div class="gallery__item"><a class="gallery__link" href="${imageOriginal[i]}"><img class="gallery__image" src ="${imagePreview[i]}" data-source="${imageOriginal[i]}"alt="${imageAlt[i]}"  ></a></div>`;
   createGalleryHTML.push(newHTML);
 }
 
-// przekształcenie tablicy w ciąg znaków
 const readyHTMLToPushToWebsite = createGalleryHTML.join(" ");
 
-//dodanie na stronę fragmentu kodu, bez pętli
 $div.insertAdjacentHTML("afterbegin", readyHTMLToPushToWebsite);
 
 $div.addEventListener("click", (ev) => {
